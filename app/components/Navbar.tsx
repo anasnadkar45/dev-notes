be7e6d1c2a23b44135dc19298f653d6fe215954c
anasnadkar45 <anasnadkar23@gmail.com>
2024-05-20 21:53:06 +0530

    updating codebase

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,29 +1,48 @@
-import Link from 'next/link'
-import React from 'react'
-import { Themetoggle } from './Themetoggle'
-import { Button } from '@/components/ui/button'
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { UserNav } from "./UserNav";
+import { Themetoggle } from "./Themetoggle";
 
-const Navbar = () => {
-    return (
-        <nav className="border-b bg-background h-[10vh] flex items-center">
-            <div className="container flex items-center justify-between">
-                <Link href="/">
-                    <h1 className="font-bold text-3xl">
-                        Dev<span className="text-primary">Notes</span>
-                    </h1>
-                </Link>
+export async function Navbar() {
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const user = await getUser();
 
-                <div className="flex items-center gap-x-5">
-                    <Themetoggle />
+  return (
+    <nav className="border-b bg-background h-[10vh] flex items-center">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <h1 className="font-bold text-3xl">
+            Marshal<span className="text-primary">Saas</span>
+          </h1>
+        </Link>
 
-                    <div className='flex items-center gap-x-5'>
-                        <Button>Sign In</Button>
-                        <Button variant={'secondary'}>Sign Up</Button>
-                    </div>
-                </div>
+        <div className="flex items-center gap-x-5">
+          <Themetoggle />
+
+          {(await isAuthenticated()) ? (
+            <UserNav
+              email={user?.email as string}
+              image={user?.picture as string}
+              name={user?.given_name as string}
+            />
+          ) : (
+            <div className="flex items-center gap-x-5">
+              <LoginLink>
+                <Button>Sign In</Button>
+              </LoginLink>
+
+              <RegisterLink>
+                <Button variant="secondary">Sign Up</Button>
+              </RegisterLink>
             </div>
-        </nav>
-    )
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
-
-export default Navbar
---
+++ app/dashboard/billing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      billing page
+    </div>
+  )
+}
+
+export default page
